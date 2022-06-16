cc9d428ae1a007edf48756b375e68aecb90b2b3a
laasyasree-glitch <laasyasree.20bcd7033@vitap.ac.in>
2022-06-16 09:11:52 +0530

    till lect 73

--- src/main/java/com/springbootproject/firstspringbootproject/SurveyController.java
+++ src/main/java/com/springbootproject/firstspringbootproject/SurveyController.java
@@ -1,12 +1,16 @@
 package com.springbootproject.firstspringbootproject;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 /*
 " / surveys / { surveyId } / questions / {questionId}"
 SurveyController
@@ -22,14 +26,36 @@ public class SurveyController {
 	private SurveyService surveyService;
 	
 	@GetMapping("/surveys/{surveyId}/questions")
-	public List<Question> retriveQuestionForSurvey(@PathVariable String surveyId){
+	public List<Question> retriveQuestionForSurvey(@PathVariable String surveyId ){
 		
 		return surveyService.retrieveQuestions(surveyId);
 	}
-	@GetMapping("/surveys/{surveyId}/questions/{questionId}")
-	public Question retriveDetailsOfQuestion(@PathVariable String surveyId,@PathVariable String questionId){
+	
+	// What should be structure of request body ?
+	// How will it be manned to Question object ? JSON=>QUESTION OBJECT
+	// What should be returned ?
+	//What should be response status ?
+	@PostMapping("/surveys/{surveyId}/questions")
+    ResponseEntity<?> add(@PathVariable String surveyId,
+            @RequestBody Question question) {
+
+        Question createdTodo = surveyService.addQuestion(surveyId, question);
+
+        if (createdTodo == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+       URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
+    
+    @GetMapping("/surveys/{surveyId}/questions/{questionId}")
+	public Question retriveQuestion(@PathVariable String surveyId,@PathVariable String questionId ){
 		
-		return surveyService.retrieveQuestion(surveyId, questionId);
+		return surveyService.retrieveQuestion(surveyId,questionId);
 	}
 
 }
--- src/main/java/com/springbootproject/firstspringbootproject/WelcomeService.java
+++ src/main/java/com/springbootproject/firstspringbootproject/WelcomeService.java
@@ -1,11 +1,14 @@
 package com.springbootproject.firstspringbootproject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service //or Component
 public class WelcomeService{
+	@Value("${welcome.message}")
+	private String welcomeMessage;
 	public String retrieveWelcomeMessage() {
 		//Complex Method
-		return "Good Morning updated!!";
+		return welcomeMessage;
 	}
 }
